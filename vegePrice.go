fb56d08a51f523e3b431183d6ea997d3767df15b
victor chassaigne <vicchassaigne@hotmail.com>
2024-07-10 09:12:08 -0400

    add addNewPrize to vege function

--- vegePrice.go
+++ vegePrice.go
@@ -3,12 +3,13 @@ package main
 import (
 	"fmt"
 	"os"
+	"slices"
 
 	"github.com/vickz86/vegePrice/data"
 )
 
 var StringToDo = "type to do\n0 exit\n1 print current prize of all vegetable\n" +
-	"2 print current price of specific vegetable"
+	"2 print current price of specific vegetable\n3 add new prize to vegetable"
 
 func main() {
 	var toDo int
@@ -28,6 +29,12 @@ func main() {
 		case 2:
 			theVeg := VegeFromList(data.VegeItem)
 			theVeg.PrintCurPrize()
+		case 3:
+			vegeAddPriz := VegeFromList(data.VegeItem)
+			newPrize := NewPrizeForVege(vegeAddPriz)
+			//create an instance with new prize
+			vegeAddPriz.AddNewPrice(newPrize)
+			data.VegeItem = AddToVegePrize(data.VegeItem, vegeAddPriz)
 		}
 
 	}
@@ -64,4 +71,36 @@ func VegeFromList(sliceVege []data.Vege) data.Vege {
 
 }
 
+// utility function
 // TODO Add new price to a vege
+func NewPrizeForVege(theVege data.Vege) float64 {
+	//variable
+	var newPrize float64
+
+	//ask new prize
+	fmt.Printf("type new prize of  %s , last prize was %.1f\n", theVege.Name, theVege.PrizeVar[len(theVege.PrizeVar)-1])
+	fmt.Scan(&newPrize)
+
+	return newPrize
+
+}
+
+// add instance with newPrize to slice of vege , return a slice of Vege with new Prize
+func AddToVegePrize(sliceVege []data.Vege, vegeToAdd data.Vege) []data.Vege {
+	//get the index of vege you want to change
+	var indexVege int
+
+	for index, vege := range sliceVege {
+		if vege.Name == vegeToAdd.Name {
+			indexVege = index
+		}
+
+	}
+
+	//replace in slice vege
+	slices.Replace(sliceVege, indexVege, indexVege+1, vegeToAdd)
+
+	//return
+	return sliceVege
+
+}
